6d74ce14e47e49ac25b111fc32325d455bc2abe5
Matthew Day <matt.day@shakerinternational.com>
2020-03-06 15:55:33 -0500

    Add src directory

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = props => {
+  return (
+    <>
+      <p>Hello World</p>
+    </>
+  );
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { render } from "react-dom";
+import "./style.scss";
+import App from "./App.js";
+
+render(<App />, document.getElementById("app"));
