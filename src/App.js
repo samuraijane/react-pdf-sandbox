fd1d165137d1ff01f0fa191290299067198182e1
Matthew Day <matt.day@shakerinternational.com>
2020-03-07 07:49:52 -0500

    Add components to view PDF

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Resume from "./components/Resume";
 
 const App = props => {
   return (
     <>
       <p>Hello World</p>
+      <Resume />
     </>
   );
 };
---
+++ src/components/Resume.js
@@ -0,0 +1,108 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import {
+  Page,
+  Text,
+  View,
+  Document,
+  PDFViewer,
+  StyleSheet
+} from "@react-pdf/renderer";
+
+// Create styles
+const styles = StyleSheet.create({
+  page: {
+    backgroundColor: "white"
+  },
+  section: {
+    margin: 10,
+    padding: 10
+  }
+});
+
+// Create Document Component
+const Resume = () => (
+  <Document>
+    <Page style={styles.page}>
+      <View style={styles.section}>
+        <Text>Section #1</Text>
+      </View>
+      <View style={styles.section}>
+        <Text>
+          En un lugar de la Mancha, de cuyo nombre no quiero acordarme, no ha
+          mucho tiempo que vivía un hidalgo de los de lanza en astillero, adarga
+          antigua, rocín flaco y galgo corredor. Una olla de algo más vaca que
+          carnero, salpicón las más noches, duelos y quebrantos los sábados,
+          lentejas los viernes, algún palomino de añadidura los domingos,
+          consumían las tres partes de su hacienda. El resto della concluían
+          sayo de velarte, calzas de velludo para las fiestas con sus pantuflos
+          de lo mismo, los días de entre semana se honraba con su vellori de lo
+          más fino. Tenía en su casa una ama que pasaba de los cuarenta, y una
+          sobrina que no llegaba a los veinte, y un mozo de campo y plaza, que
+          así ensillaba el rocín como tomaba la podadera. Frisaba la edad de
+          nuestro hidalgo con los cincuenta años, era de complexión recia, seco
+          de carnes, enjuto de rostro; gran madrugador y amigo de la caza.
+          Quieren decir que tenía el sobrenombre de Quijada o Quesada (que en
+          esto hay alguna diferencia en los autores que deste caso escriben),
+          aunque por conjeturas verosímiles se deja entender que se llama
+          Quijana; pero esto importa poco a nuestro cuento; basta que en la
+          narración dél no se salga un punto de la verdad
+        </Text>
+      </View>
+      <View style={styles.section}>
+        <Text>
+          En un lugar de la Mancha, de cuyo nombre no quiero acordarme, no ha
+          mucho tiempo que vivía un hidalgo de los de lanza en astillero, adarga
+          antigua, rocín flaco y galgo corredor. Una olla de algo más vaca que
+          carnero, salpicón las más noches, duelos y quebrantos los sábados,
+          lentejas los viernes, algún palomino de añadidura los domingos,
+          consumían las tres partes de su hacienda. El resto della concluían
+          sayo de velarte, calzas de velludo para las fiestas con sus pantuflos
+          de lo mismo, los días de entre semana se honraba con su vellori de lo
+          más fino. Tenía en su casa una ama que pasaba de los cuarenta, y una
+          sobrina que no llegaba a los veinte, y un mozo de campo y plaza, que
+          así ensillaba el rocín como tomaba la podadera. Frisaba la edad de
+          nuestro hidalgo con los cincuenta años, era de complexión recia, seco
+          de carnes, enjuto de rostro; gran madrugador y amigo de la caza.
+          Quieren decir que tenía el sobrenombre de Quijada o Quesada (que en
+          esto hay alguna diferencia en los autores que deste caso escriben),
+          aunque por conjeturas verosímiles se deja entender que se llama
+          Quijana; pero esto importa poco a nuestro cuento; basta que en la
+          narración dél no se salga un punto de la verdad
+        </Text>
+      </View>
+      <View style={styles.section}>
+        <Text>
+          En un lugar de la Mancha, de cuyo nombre no quiero acordarme, no ha
+          mucho tiempo que vivía un hidalgo de los de lanza en astillero, adarga
+          antigua, rocín flaco y galgo corredor. Una olla de algo más vaca que
+          carnero, salpicón las más noches, duelos y quebrantos los sábados,
+          lentejas los viernes, algún palomino de añadidura los domingos,
+          consumían las tres partes de su hacienda. El resto della concluían
+          sayo de velarte, calzas de velludo para las fiestas con sus pantuflos
+          de lo mismo, los días de entre semana se honraba con su vellori de lo
+          más fino. Tenía en su casa una ama que pasaba de los cuarenta, y una
+          sobrina que no llegaba a los veinte, y un mozo de campo y plaza, que
+          así ensillaba el rocín como tomaba la podadera. Frisaba la edad de
+          nuestro hidalgo con los cincuenta años, era de complexión recia, seco
+          de carnes, enjuto de rostro; gran madrugador y amigo de la caza.
+          Quieren decir que tenía el sobrenombre de Quijada o Quesada (que en
+          esto hay alguna diferencia en los autores que deste caso escriben),
+          aunque por conjeturas verosímiles se deja entender que se llama
+          Quijana; pero esto importa poco a nuestro cuento; basta que en la
+          narración dél no se salga un punto de la verdad
+        </Text>
+      </View>
+    </Page>
+  </Document>
+);
+
+const Blah = () => (
+  <PDFViewer width="1000" height="1000">
+    <Resume />
+  </PDFViewer>
+);
+
+ReactDOM.render(<Blah />, document.getElementById("pdf"));
+
+export default Blah;
